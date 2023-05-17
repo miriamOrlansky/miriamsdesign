5fbfea6404a3931a7b13d8c99e1021e4b5758541
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2023-05-17 17:25:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './src/index.css';
-import App from './src/App';
+import App from './src/App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
