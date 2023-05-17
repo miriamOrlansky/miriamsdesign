708fdc0e6300b3c3add80aa4a0d6e31934f20977
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2023-05-17 14:35:16 -0400

    add components

---
+++ src/components/CaseStudy.jsx
@@ -0,0 +1,17 @@
+import { Info, Picture, Root, Title } from "../styles/caseStudyStyles";
+
+export default function CaseStudy({ title, source, link }) {
+  return (
+    <Root>
+      <a href={link}>
+        <Picture>
+          <img src={source} alt="caseStudyImg" />
+        </Picture>
+        <Info>
+          <Title>{title}</Title>
+          <div>View Case Study</div>
+        </Info>
+      </a>
+    </Root>
+  );
+}
